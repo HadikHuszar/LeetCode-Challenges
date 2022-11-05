92b2be9c743419eef3010a0451b80fd720f60923
Hadik Huszar <86331647+HadikHuszar@users.noreply.github.com>
2022-11-05 14:43:48 -0700

    Reorganized

--- Integer to Roman (LC 12).js
+++
@@ -1,9 +0,0 @@
-var intToRoman = function (num) {
-    let M = ["", "M", "MM", "MMM"];
-    let C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-    let X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-    let I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-    return M[parseInt(num/1000)] + C[parseInt((num%1000)/100)] + X[parseInt((num%100)/10)] + I[parseInt(num%10)];
-}
-
-console.log(intToRoman(123))
